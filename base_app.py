c885ac7491abf13858b897824fcff89adb380735
siphera <salmansiphenkosi@gmail.com>
2023-12-13 17:22:17 +0200

    remove spaces

--- base_app.py
+++ base_app.py
@@ -36,7 +36,7 @@ from scipy.sparse import csr_matrix
 
 # Vectorizer
 news_vectorizer = open("resources/tfidf_vectorizer.pkl","rb")
-tweet_cv = cloudpickle .load(news_vectorizer) # loading your vectorizer from the pkl file
+tweet_cv = cloudpickle.load(news_vectorizer) # loading your vectorizer from the pkl file
 
 # Load your raw data
 raw = pd.read_csv("resources/train.csv")
@@ -107,13 +107,13 @@ def main():
 
 			# Load the selected model
 			if selected_model == "SVM":
-				predictor = cloudpickle .load(open(os.path.join("resources/svm_classifier.pkl"), "rb"))
+				predictor = cloudpickle.load(open(os.path.join("resources/svm_classifier.pkl"), "rb"))
 				prediction = predictor.predict(vect_text)
 			elif selected_model == "CNN":
-				predictor = cloudpickle .load(open(os.path.join("resources/svm_classifier.pkl"), "rb"))
+				predictor = cloudpickle.load(open(os.path.join("resources/svm_classifier.pkl"), "rb"))
 				prediction = predictor.predict(vect_text)
 			elif selected_model == "CNN2":
-				predictor = cloudpickle .load(open(os.path.join("resources/svm_classifier.pkl"), "rb"))
+				predictor = cloudpickle.load(open(os.path.join("resources/svm_classifier.pkl"), "rb"))
 				prediction = predictor.predict(vect_text)
 
 
