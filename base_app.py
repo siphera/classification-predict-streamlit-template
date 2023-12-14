85d107f86d6bd13c62490c30b1ac24de0b1ad600
siphera <salmansiphenkosi@gmail.com>
2023-12-14 11:59:05 +0200

    frequent words

--- base_app.py
+++ base_app.py
@@ -139,6 +139,11 @@ def main():
 		wordcloud_image_path = "resources/imgs/wordcloud.jpg"
 		st.image(wordcloud_image_path, use_column_width=True)
 
+		# Load and display the pre-generated word cloud image
+		st.subheader("Frequent words from each sentiment")
+		frequnt_image_path = "resources/imgs/frequent_words.png"
+		st.image(frequnt_image_path, use_column_width=True)
+
 # Required to let Streamlit instantiate our web app.  
 if __name__ == '__main__':
 	main()
